d6d85b831fe2670871a3e3807772463972edc38e
Maxime <cest-mon-yop@hotmail.fr>
2023-05-19 11:24:18 +0200

    seems finish project

--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -1,30 +1,25 @@
 
-install_wp_cli()
-{
+install_wp_cli() {
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
 }
 
-install_wp()
-{
+install_wp() {
     wp core download --path=/var/www/wordpress --force --skip-content
 }
 
-install_db_conf_wp()
-{
+install_db_conf_wp() {
     cd /var/www/wordpress
     sleep 10
     wp config create\
         --dbname=${DB_NAME} \
         --dbuser=${DB_USER} \
         --dbpass=${DB_PASSWORD} \
-        --dbhost=mariadb:3306 \
-        --dbprefix=wp_
+        --dbhost=mariadb:3306
 }
 
-config_wp()
-{
+config_wp() {
     cd /var/www/wordpress
     wp core install \
         --url=https://${DOMAIN_NAME} \
@@ -33,13 +28,14 @@ config_wp()
         --admin_password=${ADMIN_PASSWORD} \
         --admin_email=${ADMIN_EMAIL} \
         --skip-email
+
     wp user create ${USER_NAME} ${USER_EMAIL} \
-        --user_pass=${USER_PASS} \
-        --porcelain
+        --user_pass=${USER_PASS}
+    wp theme install twentytwentytwo --force
+    wp theme activate twentytwentytwo
 }
 
-main()
-{
+main() {
     install_wp_cli
     if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         install_wp
